f459ee429cfb85a8b03251262a6fb0b3fccc4a57
Shashank <mshashank02@gmail.com>
2025-05-30 15:17:50 -0700

    added more iter to linear

--- src/MAE_ViT_v2.py
+++ src/MAE_ViT_v2.py
@@ -142,7 +142,7 @@ print("🎯 Training probes...")
 X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, stratify=labels, random_state=42)
 
 # Linear Probe
-clf_linear = LogisticRegression(max_iter=1000)
+clf_linear = LogisticRegression(max_iter=5000)
 clf_linear.fit(X_train, y_train)
 acc_linear = accuracy_score(y_test, clf_linear.predict(X_test))
 
